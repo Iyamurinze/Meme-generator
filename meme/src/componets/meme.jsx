e6c69bd9dfe1fc3f2d5106bf57a4ec2a3d1da51a
Jeremie Iyamurinze <iymzjeremie307@gmail.com>
2024-09-28 15:41:57 +0200

    new commit

--- meme/src/componets/meme.jsx
+++ meme/src/componets/meme.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import memeData from "./memeData";
 
 export default function Meme(){
+    const [memeImage, setMemeImage] =React.useState("")
+
+      function getMemeimage(){
+        const memeArray = memeData.data.memes
+        const randomNumber = math.floor(math.random() * memeArray.length)
+        setMemeImage(memeArray[randomNumber].url)
+    }
+    
     return(
         <main>
-            <form>
+            <div className="form">
                 <input type="text" 
                    className="form--input" 
                        placeholder="Top text"/>
                 <input type="text" 
                     className="form--input" 
                        placeholder="Bottom text"/>
-               <botton className="form--botton">
-               Get a new meme image  🖼
-               </botton>
-            </form>
+
+               <button className="form--button">
+               Get a new meme image 🖼
+               </button>
+            </div>
+            <div>
+                <img src= {memeImage} className="meme--image"/>
+            </div>
         </main>
     )
 
---
+++ meme/src/componets/memeData.jsx
@@ -0,0 +1,32 @@
+export default{
+    "success": true,
+    "data":{
+        "meme":[
+            {
+                "id":"181913649",
+                "name":"Dake Hotline Bling",
+                "url":"https://i.imgflip.com/30b1gx.jpg",
+                "width":"1200",
+                "height":"1200",
+                "box_count":"2",                
+            },
+            {
+                "id":"181913649",
+                "name":"Dake Hotline Bling",
+                "url":"https://i.imgflip.com/30b1gx.jpg",
+                "width":"1200",
+                "height":"1200",
+                "box_count":"2",                
+            },
+            {
+                "id":"181913649",
+                "name":"Dake Hotline Bling",
+                "url":"https://i.imgflip.com/30b1gx.jpg",
+                "width":"1200",
+                "height":"1200",
+                "box_count":"2",                
+            },
+
+        ]
+    }
+}
